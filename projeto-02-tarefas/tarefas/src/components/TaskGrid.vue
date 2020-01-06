4d88048f2ac5288e6c6e90962a2b1a35934cc900
Rodrigo Tadeu <rodrigotfonsecas@outlook.com.br>
2020-01-06 18:51:49 -0300

    project finished up until session 12

---
+++ projeto-02-tarefas/tarefas/src/components/TaskGrid.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="task-grid">
+        <template v-if="tasks.length">
+            <Task v-for="(task, i) in tasks" :key="task.name"
+                @taskDeleted="$emit('taskDeleted', i)"
+                @taskStateChanged="$emit('taskStateChanged', i)"
+                :task="task"></Task>
+        </template>
+        <p v-else class="np-task"> Sua vida esta em dia :P</p>
+    </div>
+</template>
+
+<script>
+import Task from './Task.vue'
+
+export default {
+    components: { Task },
+    props: {
+        tasks: { type: Array, required: true }
+    }
+}
+</script>
+
+<style>
+    .task-grid {
+        display: flex;
+        justify-content: center;
+        flex-wrap: wrap;
+    }
+
+    .tasks-grid .task {
+        margin: 10px;
+    }
+
+    .no-task {
+        color: #aaa;
+        font-size: 1.7rem;
+    }
+</style>
