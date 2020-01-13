4f8101349622e7282dd65622a1cce55891beee4f
RodrigoTadeuF <rodrigotfonsecas@outlook.com>
2020-01-13 08:59:11 -0300

    adding projeto-03 folder

---
+++ projeto-03-acoes/stock-trader/src/data/stocks.js
@@ -0,0 +1,7 @@
+export default [
+    {id: 1, name: 'Tesla', price: 220 },
+    {id: 2, name: 'Google', price: 250 },
+    {id: 3, name: 'Amazon', price: 260 },
+    {id: 4, name: 'Apple', price: 200 },
+    {id: 5, name: 'Microsoft', price: 190 },
+]
